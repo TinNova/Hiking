4b376c737b19424e05adb19ff338b0335630b527
Tin Novakovic <tin.novakovic@comparethemarket.com>
2024-03-07 21:42:05 +0000

    Restoring the state of the app in event of system process death

--- app/src/main/java/com/tinnovakovic/hiking/presentation/HomeViewModel.kt
+++ app/src/main/java/com/tinnovakovic/hiking/presentation/HomeViewModel.kt
@@ -41,40 +41,40 @@ class HomeViewModel @Inject constructor(
         if (initializeCalled) return
         initializeCalled = true
 
-//        restoreSavedState()
-//        Log.d("TINTINTEST", "initialise")
-
+        restoreSavedState()
     }
 
-//    private fun restoreSavedState() {
-//        viewModelScope.launch {
-////            savedStateHandle.get<Set<HikingPhoto>>(SAVED_STATE_IMAGES)?.let {
-////                Log.d("TINTINTEST", "restored Images, first image: ${it.first()}")
-////                displayPhotos(it)
-////            }
-//        }
-//    }
+    private fun restoreSavedState() {
+        viewModelScope.launch {
+            savedStateHandle.get<Boolean>(SAVED_STATE_IS_START)?.let {isStart ->
+                if (isStart) {
+                    updateUiState { it.copy(isStartButton = true) }
+                    observeHikingPhotos()
+                } else {
+                    updateUiState { it.copy(isStartButton = false) }
+                    observeLocationAndFetchPhotos()
+                    observeHikingPhotos()
+                }
+            }
+        }
+    }
 
     override fun onUiEvent(event: HomeContract.UiEvents) {
         when (event) {
             is HomeContract.UiEvents.Initialise -> initialise()
 
             is HomeContract.UiEvents.StartClicked -> {
-                startLocationServiceUseCase.execute()
-
-                updateUiState {
-                    it.copy(isStartButton = false, isError = false)
-                }
-
                 observeLocationAndFetchPhotos()
                 observeHikingPhotos()
+                savedStateHandle.set(SAVED_STATE_IS_START, false)
+                updateUiState { it.copy(isStartButton = false, isError = false) }
             }
 
             is HomeContract.UiEvents.StopClicked -> {
                 stopLocationServiceUseCase.execute()
-                updateUiState {
-                    it.copy(isStartButton = true)
-                }
+                savedStateHandle.set(SAVED_STATE_IS_START, true)
+                updateUiState { it.copy(isStartButton = true) }
+
             }
 
             is HomeContract.UiEvents.OnDestroy -> stopLocationServiceUseCase.execute()
@@ -93,11 +93,12 @@ class HomeViewModel @Inject constructor(
     }
 
     private fun observeLocationAndFetchPhotos() {
+        startLocationServiceUseCase.execute()
+
         viewModelScope.launch(exceptionHandler) {
             locationInMemoryCache.cache.collect { latestLocation ->
 
                 latestLocation?.let { location ->
-                    // fetch and add photo to database
                     hikingPhotoRepository.fetchAndInsertPhoto(location)
                 }
             }
@@ -122,19 +123,25 @@ class HomeViewModel @Inject constructor(
             isStartButton = true,
             hikingPhotos = listOf(),
             scrollStateToTop = false,
-            isError = false
+            isError = false,
         )
 
-        const val SAVED_STATE_IMAGES = "saved_state_image"
+        const val SAVED_STATE_IS_START = "saved_state_is_start"
     }
 }
 
 //TODO:
 // - Observe internet state and check it inside observeLocationAndFetchPhotos()
+// - Check if compose is recomposing a lot, considering using a key with the LazyColumn
+// - Add button to reset photos/hike
+// - Add Error handling, check all error FlickrApi can send
+// - Observe state of notification and location permission
+// - Improve error handling infinite loop
+// - Improve notification messaging
+
+//DONE:
 // - Save photos in Room
 // - Have a savedStateHandle save a boolean
 //   -- After process death recovery it should be true, then display the images and start location tracking again
 // - Should database return a list of HikingPhotoEntity or HikingPhoto? Check offline app documentation
-// - Check if compose is recomposing a lot, considering using a key with the LazyColumn
-// - Add button to reset photos/hike
-// - Add Error handling, check all error FlickrApi can send
+
